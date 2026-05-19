615088ed0e097db7e3aa5857fe255d3523de784b
itznokx <dimitri.b.medeiros@gmail.com>
2026-05-18 22:17:41 -0300

    Issue #9: Generic Link Component

---
+++ components/generics/Link/index.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Text from "../Text/index"
+import NextLink from "next/link"
+interface LinkProps {
+  text:string;
+  pathName:string;
+  queryKey: string;
+  pref?: boolean;
+  size?: "sm" | "base" | "lg" | "xl" | "2xl" | "3xl" | "4xl";
+  color?: string;
+  hoverColor?:string;
+}
+
+export default Link (
+  {pathName,
+  text,
+  pref=true;
+  queryKey="",
+  size="base",
+  color="--text-primary"
+  }: LinkProps)
+  {
+  const hrefVar = queryKey ? { pathname: pathName, query: { keyword: queryKey } } : pathName;
+
+  return (
+    <>
+      <NextLink
+        href = {hrefVar}
+        prefetch= {pref}
+      >
+        <Text
+          textString={text}
+          size={size}
+          color={color}
+          hoverColor={hoverColor};
+        >
+      </NextLink>
+    </>
+  )
+}
