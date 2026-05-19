835a47ccabbd841f29bc2763b06d254b0749522f
itznokx <dimitri.b.medeiros@gmail.com>
2026-05-18 22:37:30 -0300

    Issue #12: Image generic component

---
+++ components/generics/Image/index.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Image from "next/image";
+
+interface CustomImageProps {
+  src: string;
+  alt: string;
+  size?: number;
+  shape?: "square" | "rounded";
+  className?: string;
+}
+export default function CustomImage({
+  src,
+  alt,
+  size = 64, // Tamanho padrão se não for informado
+  shape = "square",
+  className = "",
+}: CustomImageProps) {
+  // Dicionário de formatos usando classes do Tailwind
+  const shapeClasses = {
+    square: "rounded-none",
+    rounded: "rounded-md",
+  };
+
+  return (
+    <NextImage
+      src={src}
+      alt={alt}
+      width={size}
+      height={size}
+      className={`${shapeClasses[shape]} object-cover ${className}`}
+    />
+  );
+}
