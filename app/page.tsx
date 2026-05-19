946735b728ebe19e91f4c916e48ba75d90cf6eb3
VerasNp <vitorveras@proton.me>
2026-05-19 17:36:18 -0300

    WIP: Adicionando funcionalidade de butao a icone direita do input

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,169 @@
+import Icon from "@/components/generics/Icon";
 import { Input } from "@/components/generics/Input";
+import { LucideAlbum, LucideSearch } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
 	return (
-		<div className="flex justify-center h-100 bg-">
-			<div className="w-64 content-center">
-				<Input
-					variant="default"
-					placeholder="Buscar na conta ou ajuda"
-					fullWidth
-					leftIcon
-					rightIcon
-				/>
+		<>
+			<div className="flex my-7">
+				<div className="mx-5">
+					<Input
+						variant="default"
+						size="lg"
+						placeholder="Buscar na conta ou ajuda"
+						fullWidth
+						leftIcon={<Icon src={LucideSearch} />}
+						rightIcon={{
+							children: <Icon src={LucideAlbum} />,
+							action: "link",
+						}}
+					/>
+				</div>
+				<div className="mx-5">
+					<Input
+						variant="defaultFilled"
+						size="lg"
+						placeholder="Buscar na conta ou ajuda"
+						fullWidth
+						leftIcon={<Icon src={LucideSearch} />}
+						rightIcon={{
+							children: <Icon src={LucideAlbum} />,
+							action: "link",
+						}}
+					/>
+				</div>
+				<div className="mx-5">
+					<Input
+						variant="rounded"
+						size="lg"
+						placeholder="Buscar na conta ou ajuda"
+						fullWidth
+						leftIcon={<Icon src={LucideSearch} />}
+						rightIcon={{
+							children: <Icon src={LucideAlbum} />,
+							action: "link",
+						}}
+					/>
+				</div>
+				<div className="mx-5">
+					<Input
+						variant="roundedFilled"
+						size="lg"
+						placeholder="Buscar na conta ou ajuda"
+						fullWidth
+						leftIcon={<Icon src={LucideSearch} />}
+						rightIcon={{
+							children: <Icon src={LucideAlbum} />,
+							action: "link",
+						}}
+					/>
+				</div>
 			</div>
-		</div>
+			<div className="flex my-7">
+				<div className="mx-5">
+					<Input
+						variant="default"
+						placeholder="Buscar na conta ou ajuda"
+						fullWidth
+						leftIcon={<Icon src={LucideSearch} />}
+						rightIcon={{
+							children: <Icon src={LucideAlbum} />,
+							action: "link",
+						}}
+					/>
+				</div>
+				<div className="mx-5">
+					<Input
+						variant="defaultFilled"
+						placeholder="Buscar na conta ou ajuda"
+						fullWidth
+						leftIcon={<Icon src={LucideSearch} />}
+						rightIcon={{
+							children: <Icon src={LucideAlbum} />,
+							action: "link",
+						}}
+					/>
+				</div>
+				<div className="mx-5">
+					<Input
+						variant="rounded"
+						placeholder="Buscar na conta ou ajuda"
+						fullWidth
+						leftIcon={<Icon src={LucideSearch} />}
+						rightIcon={{
+							children: <Icon src={LucideAlbum} />,
+							action: "link",
+						}}
+					/>
+				</div>
+				<div className="mx-5">
+					<Input
+						variant="roundedFilled"
+						placeholder="Buscar na conta ou ajuda"
+						fullWidth
+						leftIcon={<Icon src={LucideSearch} />}
+						rightIcon={{
+							children: <Icon src={LucideAlbum} />,
+							action: "link",
+						}}
+					/>
+				</div>
+			</div>
+			<div className="flex my-7">
+				<div className="mx-5">
+					<Input
+						variant="default"
+						size="sm"
+						placeholder="Buscar na conta ou ajuda"
+						fullWidth
+						leftIcon={<Icon src={LucideSearch} />}
+						rightIcon={{
+							children: <Icon src={LucideAlbum} />,
+							action: "link",
+						}}
+					/>
+				</div>
+				<div className="mx-5">
+					<Input
+						variant="defaultFilled"
+						size="sm"
+						placeholder="Buscar na conta ou ajuda"
+						fullWidth
+						leftIcon={<Icon src={LucideSearch} />}
+						rightIcon={{
+							children: <Icon src={LucideAlbum} />,
+							action: "link",
+						}}
+					/>
+				</div>
+				<div className="mx-5">
+					<Input
+						variant="rounded"
+						size="sm"
+						placeholder="Buscar na conta ou ajuda"
+						fullWidth
+						leftIcon={<Icon src={LucideSearch} />}
+						rightIcon={{
+							children: <Icon src={LucideAlbum} />,
+							action: "link",
+						}}
+					/>
+				</div>
+				<div className="mx-5">
+					<Input
+						variant="roundedFilled"
+						size="sm"
+						placeholder="Buscar na conta ou ajuda"
+						fullWidth
+						leftIcon={<Icon src={LucideSearch} />}
+						rightIcon={{
+							children: <Icon src={LucideAlbum} />,
+							action: "link",
+						}}
+					/>
+				</div>
+			</div>
+		</>
 	);
 }
--- components/generics/Input/index.tsx
+++ components/generics/Input/index.tsx
@@ -7,8 +7,17 @@ function cn(...inputs: ClassValue[]) {
 }
 
 export type InputSize = "sm" | "md" | "lg";
-export type InputVariant = "default" | "filled" | "ghost";
+export type InputVariant =
+	| "default"
+	| "defaultFilled"
+	| "rounded"
+	| "roundedFilled";
 export type InputState = "idle" | "error" | "success" | "loading";
+
+interface InputRightIconProps {
+	children: ReactNode;
+	action: "link" | "button";
+}
 
 export interface InputProps extends Omit<
 	InputHTMLAttributes<HTMLInputElement>,
@@ -20,22 +29,21 @@ export interface InputProps extends Omit<
 	label?: string;
 	helperText?: string;
 	leftIcon?: ReactNode;
-	rightIcon?: ReactNode;
+	rightIcon?: InputRightIconProps;
 	fullWidth?: boolean;
 }
 
 const variantStyles: Record<InputVariant, string> = {
 	default:
-		"bg-[var(--bg-elevated-main)] text-primary border-[var(--bg-secondary)]" +
-		"focus:border-spotify-green focus:ring-1 focus:ring-spotify-green",
-	filled:
-		"bg-spotify-elevated border border-transparent text-spotify-white " +
-		"placeholder:text-spotify-muted " +
-		"focus:border-spotify-green focus:ring-1 focus:ring-spotify-green",
-	ghost:
-		"bg-transparent border-0 border-b border-spotify-muted text-spotify-white " +
-		"placeholder:text-spotify-muted rounded-none px-0 " +
-		"focus:border-b-spotify-green focus:ring-0",
+		"bg-[var(--bg-elevated-main)] text-primary border-[var(--bg-secondary)]",
+	defaultFilled:
+		"bg-[var(--bg-elevated-main)] border-[var(--bg-secondary)] text-primary " +
+		"focus:ring-1",
+	rounded:
+		"bg-[var(--bg-elevated-main)] text-primary border-[var(--bg-secondary)] rounded-full ",
+	roundedFilled:
+		"bg-[var(--bg-elevated-main)] border-[var(--bg-secondary)] text-primary rounded-full " +
+		"focus:ring-1",
 };
 
 const sizeStyles: Record<InputSize, string> = {
@@ -44,10 +52,25 @@ const sizeStyles: Record<InputSize, string> = {
 	lg: "h-14 text-base px-5",
 };
 
-const iconSizeStyles: Record<InputSize, string> = {
-	sm: "w-3.5 h-3.5",
-	md: "w-4 h-4",
-	lg: "w-5 h-5",
+const iconSizeStyles: Record<
+	InputSize,
+	{ size: string; leftPadding: string; rightPadding: string }
+> = {
+	sm: {
+		size: "w-3.5 h-3.5",
+		leftPadding: "pl-8",
+		rightPadding: "pr-8",
+	},
+	md: {
+		size: "w-4 h-4",
+		leftPadding: "pl-9",
+		rightPadding: "pr-9",
+	},
+	lg: {
+		size: "w-6 h-6",
+		leftPadding: "pl-12",
+		rightPadding: "pr-12",
+	},
 };
 
 const stateStyles: Record<InputState, string> = {
@@ -63,7 +86,7 @@ const helperTextColorStyles: Record<InputState, string> = {
 	success: "text-green-400",
 	loading: "text-spotify-muted",
 };
-const EMPTY_ICON = `data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg'/%3E`;
+
 const Input = forwardRef<HTMLInputElement, InputProps>(
 	(
 		{
@@ -105,11 +128,11 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
 					{leftIcon && (
 						<span
 							className={cn(
-								"absolute left-3 flex items-center justify-center text-spotify-muted pointer-events-none bg-white",
-								iconSizeStyles[size],
+								"absolute left-3 flex items-center justify-center text-spotify-muted pointer-events-none",
+								iconSizeStyles[size].size,
 							)}
 						>
-							<img src={EMPTY_ICON} width="100%" height="100%" />
+							{leftIcon}
 						</span>
 					)}
 					<input
@@ -122,22 +145,24 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
 							variantStyles[variant],
 							sizeStyles[size],
 							stateStyles[state],
-							leftIcon && "pl-9",
-							rightIcon && "pr-9",
+							leftIcon && iconSizeStyles[size].leftPadding,
+							rightIcon && iconSizeStyles[size].rightPadding,
 							className,
 						)}
 						{...props}
 					/>
 
 					{rightIcon && (
-						<span
-							className={cn(
-								"absolute right-3 flex items-center justify-center text-spotify-muted bg-white",
-								iconSizeStyles[size],
-							)}
-						>
-							<img src={EMPTY_ICON} width="100%" height="100%" />
-						</span>
+						<a href="www.google.com">
+							<span
+								className={cn(
+									"absolute right-3 flex items-center justify-center text-spotify-muted",
+									iconSizeStyles[size].size,
+								)}
+							>
+								{rightIcon.children}
+							</span>
+						</a>
 					)}
 				</div>
 
