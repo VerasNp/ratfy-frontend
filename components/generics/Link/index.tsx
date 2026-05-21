0ebd584a83aad34a9251e472fcb349759e7d2eaf
VerasNp <vitorveras@proton.me>
2026-05-21 18:49:26 -0300

    fix: Corrige problemas com link
    reff.: #9

--- components/generics/Link/index.tsx
+++ components/generics/Link/index.tsx
@@ -1,40 +1,39 @@
 import React from "react";
-import Text from "../Text/index"
-import NextLink from "next/link"
+import Text from "../Text/index";
+import NextLink from "next/link";
 interface LinkProps {
-  text:string;
-  pathName:string;
-  queryKey: string;
-  pref?: boolean;
-  size?: "sm" | "base" | "lg" | "xl" | "2xl" | "3xl" | "4xl";
-  color?: string;
-  hoverColor?:string;
+	text: string;
+	pathName: string;
+	queryKey: string;
+	pref?: boolean;
+	size?: "sm" | "base" | "lg" | "xl" | "2xl" | "3xl" | "4xl";
+	color?: string;
+	hoverColor?: string;
 }
 
-export default Link (
-  {pathName,
-  text,
-  pref=true,
-  queryKey="",
-  size="base",
-  color="--text-primary"
-  }: LinkProps)
-  {
-  const hrefVar = queryKey ? { pathname: pathName, query: { keyword: queryKey } } : pathName;
+export default function Link({
+	pathName,
+	text,
+	pref = true,
+	queryKey = "",
+	size = "base",
+	color = "--text-primary",
+	hoverColor = "--text-primary-hover"
+}: LinkProps) {
+	const hrefVar = queryKey
+		? { pathname: pathName, query: { keyword: queryKey } }
+		: pathName;
 
-  return (
-    <>
-      <NextLink
-        href = {hrefVar}
-        prefetch= {pref}
-      >
-        <Text
-          textString={text}
-          size={size}
-          color={color}
-          hoverColor={hoverColor};
-        >
-      </NextLink>
-    </>
-  )
+	return (
+		<>
+			<NextLink href={hrefVar} prefetch={pref}>
+				<Text
+					textString={text}
+					size={size}
+					color={color}
+					hoverColor={hoverColor}
+				/>
+			</NextLink>
+		</>
+	);
 }
