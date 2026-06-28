ba0b4a880f74145de93e7bb3fdc014f663ae647a
itznokx <dimitri.b.medeiros@gmail.com>
2026-06-28 14:07:46 -0300

    chore(queue): add card group test

---
+++ app/test/CardTest.tsx
@@ -0,0 +1,180 @@
+import Card from "@/components/generics/Card";
+import CardGroup from "@/components/generics/CardGroup";
+import Placeholder from "@/public/placeholder_1024.jpg"
+
+export default function CardTest() {
+  return (
+    <>
+      <CardGroup
+        title="Teste 2"
+        orientation="horizontal"
+      >
+        <Card
+          title="Churrasquinho 3 ( Ao Vivo )"
+          subtitle=""
+          imageSrc={Placeholder}
+          cardType="Single"
+          cardOwner="Grupo Menos É Mais"
+          orientation="vertical"
+        ></Card>
+        <Card
+          title="Fly Out West - Single Version"
+          subtitle=""
+          imageSrc={Placeholder}
+          cardType="Single"
+          cardOwner="Yot club"
+          orientation="vertical"
+        ></Card>
+        <Card
+          title="Heads Will Roll - A-Trak Remix"
+          subtitle=""
+          imageSrc={Placeholder}
+          cardType="Single"
+          cardOwner="Yot club"
+          orientation="vertical"
+        ></Card>
+        <Card
+          title="CHAMO TEU VULGO MALVADÃO, METE COM FORÇA - MC JHENNY [ DJ KOSTA VINTE DOIS ] 130 BPM"
+          subtitle=""
+          imageSrc={Placeholder}
+          cardType="Single"
+          cardOwner="PL TORVIC"
+          orientation="vertical"
+        ></Card>
+        <Card
+          title="Billie Jean"
+          subtitle=""
+          imageSrc={Placeholder}
+          cardType="Single"
+          cardOwner="Michael Jackson"
+          orientation="vertical"
+        ></Card>
+        <Card
+          title="Stressed Out"
+          subtitle=""
+          imageSrc={Placeholder}
+          cardType="Single"
+          cardOwner="Twenty One Pilots"
+          orientation="vertical"
+        ></Card>
+        <Card
+          title="Essência da Paixão"
+          subtitle=""
+          imageSrc={Placeholder}
+          cardType="Single"
+          cardOwner="Grupo Revelação"
+          orientation="vertical"
+        ></Card>
+        <Card
+          title="Até que durou"
+          subtitle=""
+          imageSrc={Placeholder}
+          cardType="Single"
+          cardOwner="Péricles"
+          orientation="vertical"
+        ></Card>
+        <Card
+          title="Funk do Pão de Queijo"
+          subtitle=""
+          imageSrc={Placeholder}
+          cardType="Single"
+          cardOwner="Bolofofos"
+          orientation="vertical"
+        ></Card>
+        <Card
+          title="Viajar pelo Safari"
+          subtitle=""
+          imageSrc={Placeholder}
+          cardType="Single"
+          cardOwner="Mundo Bita"
+          orientation="vertical"
+        ></Card>
+      </CardGroup>
+      <CardGroup
+        title="Populares"
+        orientation="horizontal"
+      >
+        <Card
+          title="Churrasquinho 3 ( Ao Vivo )"
+          subtitle=""
+          imageSrc={Placeholder}
+          cardType="Single"
+          cardOwner="Grupo Menos É Mais"
+          orientation="vertical"
+        ></Card>
+        <Card
+          title="Fly Out West - Single Version"
+          subtitle=""
+          imageSrc={Placeholder}
+          cardType="Single"
+          cardOwner="Yot club"
+          orientation="vertical"
+        ></Card>
+        <Card
+          title="Heads Will Roll - A-Trak Remix"
+          subtitle=""
+          imageSrc={Placeholder}
+          cardType="Single"
+          cardOwner="Yot club"
+          orientation="vertical"
+        ></Card>
+        <Card
+          title="CHAMO TEU VULGO MALVADÃO, METE COM FORÇA - MC JHENNY [ DJ KOSTA VINTE DOIS ] 130 BPM"
+          subtitle=""
+          imageSrc={Placeholder}
+          cardType="Single"
+          cardOwner="PL TORVIC"
+          orientation="vertical"
+        ></Card>
+        <Card
+          title="Billie Jean"
+          subtitle=""
+          imageSrc={Placeholder}
+          cardType="Single"
+          cardOwner="Michael Jackson"
+          orientation="vertical"
+        ></Card>
+        <Card
+          title="Stressed Out"
+          subtitle=""
+          imageSrc={Placeholder}
+          cardType="Single"
+          cardOwner="Twenty One Pilots"
+          orientation="vertical"
+        ></Card>
+        <Card
+          title="Essência da Paixão"
+          subtitle=""
+          imageSrc={Placeholder}
+          cardType="Single"
+          cardOwner="Grupo Revelação"
+          orientation="vertical"
+        ></Card>
+        <Card
+          title="Até que durou"
+          subtitle=""
+          imageSrc={Placeholder}
+          cardType="Single"
+          cardOwner="Péricles"
+          orientation="vertical"
+        ></Card>
+        <Card
+          title="Funk do Pão de Queijo"
+          subtitle=""
+          imageSrc={Placeholder}
+          cardType="Single"
+          cardOwner="Bolofofos"
+          orientation="vertical"
+        ></Card>
+        <Card
+          title="Viajar pelo Safari"
+          subtitle=""
+          imageSrc={Placeholder}
+          cardType="Single"
+          cardOwner="Mundo Bita"
+          orientation="vertical"
+        ></Card>
+      </CardGroup>
+    </>
+  );
+}
