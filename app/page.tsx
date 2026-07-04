8c3f86ef86f8d4ec5981fc6c6f9540fe1f29209c
itznokx <dimitri.b.medeiros@gmail.com>
2026-07-04 02:00:39 -0300

    fix(sidebar): fix card items not executing dropdown actions

--- app/page.tsx
+++ app/page.tsx
@@ -5,55 +5,34 @@ import CardTest from "./test/CardTest";
 import ContentPageTest from "./test/ContentPageTest";
 import Sidebar from "@/components/generics/Sidebar"
 import { mockedPlaylist,mockedPlaylists,mockedAlbums } from "./test/TrackList";
-import { usePlayer } from "./context/PlayerContext";
-import { Track } from "@/components/generics/Player";
-import { Play, ListPlus } from "lucide-react"
-import { DropdownItem } from "@/components/generics/Card";
+import { SidebarItem } from "@/components/generics/Sidebar";
 
 export default function Home() {
   const popularTracks = mockedPlaylist.slice(0, 40);
-  const { playNow, playNext, addToQueue } = usePlayer();
-  const cardDropdownActions = (tracks: Track[]): DropdownItem[] => [
-    {
-      id: "play-now",
-      icon: <Play size={16} />,
-      item: <span>Play Now</span>,
-      onSelect: () => playNow(tracks),
-    },
-    {
-      id: "Play next",
-      icon: <ListPlus size={16} />,
-      item: <span>Next in queue</span>,
-      onSelect: () => playNext(tracks),
-    },
-    {
-      id: "add-queue",
-      icon: <ListPlus size={16} />,
-      item: <span>Add to Queue</span>,
-      onSelect: () => addToQueue(tracks),
-    },
-  ];
   const sidebarData: SidebarItem[] = [
     ...mockedPlaylists.map(p => ({
       id: p.id,
       title: p.title,
       type: "Playlist" as const,
       imageUrl: p.albumArtUrl,
-      owner: Array.isArray(p.artistsIds) ? p.artistsIds.join(", ") : p.artistsIds
+      owner: Array.isArray(p.artistsIds) ? p.artistsIds.join(", ") : p.artistsIds,
+      tracks: p.tracks
     })),
     ...mockedAlbums.map(a => ({
       id: a.id,
       title: a.title,
       type: "Album" as const,
       imageUrl: a.albumArtUrl,
-      owner: "Various Artists"
+      owner: "Various Artists",
+      tracks: a.tracks
     })),
     ...popularTracks.map(t => ({
       id: t.id,
       title: t.title,
       type: "Single" as const,
       imageUrl: t.albumArtUrl,
-      owner: t.artist
+      owner: t.artist,
+      tracks: [t]
     }))
   ];
   return (
