d0295ff41f828d45e1f4bc5ffcbbec13893b0ac9
itznokx <dimitri.b.medeiros@gmail.com>
2026-07-05 15:12:05 -0300

    fix(layout): include Sidebar and Player in Layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,8 @@ import "./globals.css";
 import { PlayerProvider } from "./context/PlayerContext";
 import { UserProvider } from "./context/UserContext";
 import NavBar from "@/components/generics/NavBar";
-
+import Sidebar from "@/components/generics/Sidebar"
+import Player from "@/components/generics/Player";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -33,8 +34,14 @@ export default function RootLayout({
       <body className="h-screen flex flex-col overflow-y-hidden">
         <PlayerProvider>
           <UserProvider>
-            <NavBar/>
-            {children}
+            <NavBar />
+            <main className="min-h-screen bg-linear-to-b from-gray-900 to-black text-white pb-32">
+              <div className="flex flex-row h-[calc(100vh-150px)]">
+                <Sidebar/>
+                {children}
+              </div>
+              <Player />
+            </main>
           </UserProvider>
         </PlayerProvider>
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,12 @@
 import Player from "@/components/generics/Player";
 import CardTest from "./test/CardTest";
 import ContentPageTest from "./test/ContentPageTest";
-import Sidebar from "@/components/generics/Sidebar"
+
 
 export default function Home() {
   return (
       <main className="min-h-screen bg-linear-to-b from-gray-900 to-black text-white pb-32">
         <div className="flex flex-row h-[calc(100vh-150px)]">
-          <Sidebar />
           <div className="flex flex-col overflow-y-scroll">
             <ContentPageTest/>
             <CardTest/>
